e2b3e2ccac08b0c8e7a420c47475e7c0e6780f14
ashmina <raiashmina404@gmail.com>
2021-08-29 19:25:35 +0545

    updated

---
+++ src/Components/Basics/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./style.css"
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="navigation-content">
+                <div className="content">
+                    <h4>Yamphu</h4>
+                </div>
+
+                <div className="content">
+                    <h4>Yamphu</h4>
+                </div>
+
+                <div className="content">
+                    <h4>Yamphu</h4>
+                </div>
+
+                <div className="content">
+                    <h4>Yamphu</h4>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
+
