9a6727fd35aaf51a5ecca8de2b372ffaafb8c48e
ashmina <raiashmina404@gmail.com>
2021-08-30 16:31:50 +0545

    updated

--- src/Components/Basics/Info.js
+++ src/Components/Basics/Info.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Info = () => {
     return (
-        <div>
-            <img src="back.jpg"></img>
-        </div>
+        <>
+            <div className="info-content">
+                <div className="info">
+                    <img src="profile.png" className="profile"></img>
+                </div>
+                <div className="info-tech">
+                    <h2>Yamphuashmina</h2>
+                    <h6>minimal and cozy designs</h6>
+                    <h6>Sajilo software | 2021</h6>
+                </div>
+                <div className="info">
+                    <img src="profile.png" className="pro"></img>
+                    <h6 class="ytext">Yamphu</h6>
+                    <h6 class="ctext">Contact me</h6>
+                </div>
+            </div>
+        </>
     )
 }
 
