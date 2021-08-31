074254b7b0d07c7723146146afe8dd4b723c3b78
ashmina <raiashmina404@gmail.com>
2021-08-31 15:13:00 +0545

    updated

--- src/Components/Basics/Items.js
+++ src/Components/Basics/Items.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const Items = () => {
-    return (
-        <>
-            <div className="item">
-                <h3 className="it">Items</h3>
-            </div>
+  return (
+    <>
+      <div className="item">
+        <div class="icont">
+          <h3 className="it">Items</h3>
+        </div>
+        <div className="dropdown">
+          <div className="dropcontent">
+              <span className="sort">Sort: most recent</span>
+              <i className="fa fa-caret-down"></i>
+          </div>
 
-            <div className="items-content">
-                <div className="icontent">
-                    <input type="text" placeholder="Search items" className="itext" />
-                </div>
+        </div>
+      </div>
 
-                <div className="search">
-                    <i class="fas fa-search"></i>
-                </div>
-            </div>
-        </>
-    )
-}
+      <div className="items-content">
+        <div className="icontent">
+          <input type="text" placeholder="Search items" className="itext" />
+        </div>
+
+        <div className="search">
+          <i class="fas fa-search"></i>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Items;
