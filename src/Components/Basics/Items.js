d9eb6f4e67ff145ca253a77f4be7085392fec60d
ashmina <raiashmina404@gmail.com>
2021-09-02 13:21:47 +0545

    updated

--- src/Components/Basics/Items.js
+++ src/Components/Basics/Items.js
@@ -38,39 +38,104 @@ const Items = () => {
               <a href="#">Arts</a>
               <br></br>
               <a href="#">Deskplanner</a>
+              <h6 className="owner">contact</h6>
+              <hr className="hritem"></hr>
             </a>
           </span>
         </div>
+
+        {/* images  start */}
 
         <div className="images-content">
           <div className="imgcontent">
             <div className="images">
               <img src="desk.jpg" className="desk"></img>
+              <div className="txt">
+                <h6>Card</h6>
+              </div>
               <div className="heart">
                 <span>$100</span>
-                <i className = "fas fa-heart"></i>
+                <i className="fas fa-heart"></i>
               </div>
             </div>
-            
-            <div className="images">
-              <img src="desk.jpg" className="desk"></img>
-              <div className="heart">
-                <span>$100</span>
-                <i className = "fas fa-heart"></i>
-              </div>
-            </div>
+
+            {/* images end */}
 
             <div className="images">
               <img src="desk.jpg" className="desk"></img>
+              <div className="txt">
+                <h6>Card</h6>
+              </div>
               <div className="heart">
                 <span>$100</span>
-                <i className = "fas fa-heart"></i>
+                <i className="fas fa-heart"></i>
               </div>
             </div>
 
+            <div className="images">
+              <img src="desk.jpg" className="desk"></img>
+              <div className="txt">
+                <h6>Card</h6>
+              </div>
+              <div className="heart">
+                <span>$100</span>
+                <i className="fas fa-heart"></i>
+              </div>
+            </div>
 
+            <div className="images">
+              <img src="desk.jpg" className="desk"></img>
+              <div className="txt">
+                <h6>Card</h6>
+              </div>
+              <div className="heart">
+                <span>$100</span>
+                <i className="fas fa-heart"></i>
+              </div>
+            </div>
           </div>
-        </div>  
+        </div>
+      </div>
+
+      {/* item-content ends */}
+
+      {/* second-item content starts */}
+
+      <div className="second-content">
+        <div className="simg-content">
+          <div className="admirer">
+            <u><a href="#">293 Sales</a></u>
+            <br></br>
+            <u><a href="#">290 admirers</a></u>
+            <hr className="hr"></hr>
+          </div>
+          <div className="simages">
+            <img src="mob.jpg" className="ipad"></img>
+            <div className="txt">
+                <h6>Keyboard</h6>
+              </div>
+            <div className="dollar">
+              <span>$70</span>
+              <i className="fas fa-heart"></i>
+            </div>
+          </div>
+
+          <div className="s-images">
+            <img src="mob.jpg" className="ipad"></img>
+            <div className="txt">
+                <h6>Keyboard</h6>
+              </div>
+            <div className="dollar">
+              <span>$70</span>
+              <i className="fas fa-heart"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className="report">
+          <u><h6>Report this shop to yamphu</h6></u>
+        </div>
+        <hr className="reporthr"></hr>
       </div>
     </>
   );
