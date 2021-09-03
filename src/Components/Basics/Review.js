b068f7e14f12f0b0ae91085fd5b7b78cd1724899
ashmina <raiashmina404@gmail.com>
2021-09-03 15:43:23 +0545

    updated

--- src/Components/Basics/Review.js
+++ src/Components/Basics/Review.js
@@ -13,18 +13,24 @@ const Review = () => {
             
             {/* content start */}
             <div className="review-list">
-                <div className="review">
+                <div className="rfeedback">
                     <h3>Feedback</h3>
                 </div>
             </div>
             {/* content end */}
 
             {/* content start */}
-            <div className="review-list">
-                <div className="review">
-                    <h3>Sort of: </h3>
-                    <div>
-                        wdsfjhei
+            <div className="sdrop">
+                <div className="dropd">
+                    <div className="rdown">
+                    <span className="latest">Sort of:</span>
+                    <i className="fas fa-caret-down rcaret"/>
+                    </div>
+                    <div className="list">
+                        <a href="#">Latest</a><br/>
+                        <hr className="lhr"/>
+                        <a href="#">Recent</a>
+                        <hr className="lhr"/>
                     </div>
                 </div>
             </div>
