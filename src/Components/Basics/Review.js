0113718bf8f8b7fed2295b06ea4b95aa13c21221
ashmina <raiashmina404@gmail.com>
2021-09-03 15:04:31 +0545

    updated

--- src/Components/Basics/Review.js
+++ src/Components/Basics/Review.js
@@ -3,9 +3,36 @@ import React from 'react'
 const Review = () => {
     return (
         <>
-        <div>
+        {/* start */}
+        <div className="review-content">
+            <div className="review-list">
+                <div className="review">
+                    <h3>Review</h3>
+                </div>
+            </div>
+            
+            {/* content start */}
+            <div className="review-list">
+                <div className="review">
+                    <h3>Feedback</h3>
+                </div>
+            </div>
+            {/* content end */}
+
+            {/* content start */}
+            <div className="review-list">
+                <div className="review">
+                    <h3>Sort of: </h3>
+                    <div>
+                        wdsfjhei
+                    </div>
+                </div>
+            </div>
+
+            {/* content end */}
             
         </div>
+        {/* ends */}
         </>
     )
 }
